ee32e24e1702e4005ebb75887df5f34b5cd2c9b2
bogo8liuk <lucaborghi99@gmail.com>
2026-08-11 23:46:27 +0200

    feat: Frere Jacques experiments

--- src/FrereJacques.hs
+++ src/FrereJacques.hs
@@ -1,31 +1,49 @@
 module FrereJacques
   ( frereJacques
+  , frereJacquesRmx1
+  , halfFrereJacques2
 ) where
 
 import Lib
 
 frereJacques :: Music Pitch
 frereJacques = chord [trebleClefMel, bassClefMel]
-  where
-    trebleClefMel = line [trebleClefMel1, trebleClefMel1, trebleClefMel2,
-      trebleClefMel2, trebleClefMel3, trebleClefMel3, trebleClefMel4,
-      trebleClefMel4]
 
-    trebleClefMel1 = line [c 4 qn, d 4 qn, e 4 qn, c 4 qn]
+frereJacquesRmx1 :: Music Pitch
+frereJacquesRmx1 = line [frereJacques, tempo (160/120) frereJacques,
+  tempo (200/120) frereJacques, times 2 (tempo (200/120) halfFrereJacques2)]
 
-    trebleClefMel2 = line [e 4 qn, f 4 qn, g 4 hn]
+halfFrereJacques2 :: Music Pitch
+halfFrereJacques2 = retro . cut (bn * 2) $ retro frereJacques
 
-    trebleClefMel3 = line [g 4 den, a 4 sn, g 4 en, f 4 en, e 4 qn, c 4 qn]
+trebleClefMel :: Music Pitch
+trebleClefMel = line [times 2 trebleClefMel1, times 2 trebleClefMel2,
+  times 2 trebleClefMel3, times 2 trebleClefMel4]
 
-    trebleClefMel4 = line [c 4 qn, g 4 qn, c 4 hn]
+trebleClefMel1 :: Music Pitch
+trebleClefMel1 = line [c 4 qn, d 4 qn, e 4 qn, c 4 qn]
 
-    bassClefMel = line [bassClefMel1, bassClefMel1, bassClefMel2, bassClefMel2,
-      bassClefMel3, bassClefMel3, bassClefMel4, bassClefMel4]
+trebleClefMel2 :: Music Pitch
+trebleClefMel2 = line [e 4 qn, f 4 qn, g 4 hn]
 
-    bassClefMel1 = line [c 3 hn, e 3 hn]
+trebleClefMel3 :: Music Pitch
+trebleClefMel3 = line [g 4 den, a 4 sn, g 4 en, f 4 en, e 4 qn, c 4 qn]
 
-    bassClefMel2 = line [e 3 hn, g 3 hn]
+trebleClefMel4 :: Music Pitch
+trebleClefMel4 = line [c 4 qn, g 4 qn, c 4 hn]
 
-    bassClefMel3 = line [g 3 hn, e 3 hn]
+bassClefMel :: Music Pitch
+bassClefMel = line [times 2 bassClefMel1, times 2 bassClefMel2,
+  times 2 bassClefMel3, times 2 bassClefMel4]
 
-    bassClefMel4 = line [rest qn, g 3 hn, rest qn]
+bassClefMel1 :: Music Pitch
+bassClefMel1 = line [c 3 hn, e 3 hn]
+
+bassClefMel2 :: Music Pitch
+bassClefMel2 = line [e 3 hn, g 3 hn]
+
+bassClefMel3 :: Music Pitch
+bassClefMel3 = line [g 3 hn, e 3 hn]
+
+bassClefMel4 :: Music Pitch
+bassClefMel4 = line [rest qn, g 3 hn, rest qn]
--- src/Rythm.hs
+++ src/Rythm.hs
@@ -1,5 +1,7 @@
 module Rythm
   ( basicRythm
+  , pr1
+  , pr2
 ) where
 
 import Euterpea
@@ -11,3 +13,24 @@ basicRythm = repeatN metronome
     metronome = metro :+: metro :+: metro :+: metro
 
     metro = instrument Percussion $ a 4 den
+
+pr1, pr2 :: Pitch -> Music Pitch
+pr1 p = tempo (5/6)
+  (tempo (4/3)
+    (mkLn 1 p qn :+:
+    tempo (3/2) (
+      mkLn 3 p en :+:
+      mkLn 2 p sn :+:
+      mkLn 1 p qn
+    ) :+:
+    mkLn 1 p qn
+    ) :+:
+  tempo (3/2) (mkLn 6 p en)
+  )
+pr2 p =
+  let m1 = tempo (5/4) (tempo (3/2) m2 :+: m2 )
+      m2 = mkLn 3 p en
+  in tempo (7/6) (m1 :+: tempo (5/4) (mkLn 5 p en) :+: m1 :+: tempo (3/2) m2)
+
+mkLn :: Int -> a -> Dur -> Music a
+mkLn n p d = line $ take n $ repeat $ note d p
