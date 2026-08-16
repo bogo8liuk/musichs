4536afd1c7b5ecf6735a74c41248de258163a11c
bogo8liuk <lucaborghi99@gmail.com>
2026-08-16 22:12:47 +0200

    feat: define some drums

--- src/Rythm.hs
+++ src/Rythm.hs
@@ -1,36 +1,17 @@
 module Rythm
-  ( basicRythm
-  , pr1
-  , pr2
+  ( basicDrums
+  , accompanyingDrums
 ) where
 
 import Euterpea
 import Lib
 
-basicRythm :: Int -> Music Pitch
-basicRythm = repeatN metronome
+basicDrums :: Music Pitch
+basicDrums = repeatM $ roll en $ perc ClosedHiHat 2
+
+accompanyingDrums :: Music Pitch
+accompanyingDrums = repeatM drums
   where
-    metronome = metro :+: metro :+: metro :+: metro
+    drums =  line [enr, enr, drumsHit sn, snr, enr, enr, enr, drumsHit en, enr]
 
-    metro = instrument Percussion $ a 4 den
-
-pr1, pr2 :: Pitch -> Music Pitch
-pr1 p = tempo (5/6)
-  (tempo (4/3)
-    (mkLn 1 p qn :+:
-    tempo (3/2) (
-      mkLn 3 p en :+:
-      mkLn 2 p sn :+:
-      mkLn 1 p qn
-    ) :+:
-    mkLn 1 p qn
-    ) :+:
-  tempo (3/2) (mkLn 6 p en)
-  )
-pr2 p =
-  let m1 = tempo (5/4) (tempo (3/2) m2 :+: m2 )
-      m2 = mkLn 3 p en
-  in tempo (7/6) (m1 :+: tempo (5/4) (mkLn 5 p en) :+: m1 :+: tempo (3/2) m2)
-
-mkLn :: Int -> a -> Dur -> Music a
-mkLn n p d = line $ take n $ repeat $ note d p
+    drumsHit = perc ElectricSnare
