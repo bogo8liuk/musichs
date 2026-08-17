8827155f7e6ed6a047902a2fcb7cff615f36efc0
bogo8liuk <lucaborghi99@gmail.com>
2026-08-17 11:31:23 +0200

    feat: experimenting some beats

---
+++ src/SomeBeats.hs
@@ -0,0 +1,32 @@
+module SomeBeats
+  ( beat1
+  , beat2
+) where
+
+import Lib
+import Rythm
+
+beat1 :: Music Pitch
+beat1 = cut 16 beat
+  where
+    beat = instrument HammondOrgan $ repeatM t251 :=: (basicDrums :=: accompanyingDrums)
+
+beat2 :: Music Pitch
+beat2 = cut 16 $ melody :=: beat1
+  where
+    melody = repeatM $ instrument Xylophone $ line [wnr, n1, n2, n3, n4]
+
+    n1 = chord [f 5 en, d 5 en, g 4 en]
+
+    n2 = chord [f 5 en, bf 5 en, g 4 en]
+
+    n3 = chord [f 5 en, e 5 en, g 4 en]
+
+    n4 = n1
+
+t251 :: Music Pitch
+t251 =
+    let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+        gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
+        cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+    in dMinor :+: gMajor :+: cMajor
