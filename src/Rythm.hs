9c1782e4e94faa6af2863da8743ae51411d3c3db
bogo8liuk <lucaborghi99@gmail.com>
2026-08-17 15:38:44 +0200

    feat: trap beat

--- src/Rythm.hs
+++ src/Rythm.hs
@@ -1,6 +1,7 @@
 module Rythm
   ( basicDrums
   , accompanyingDrums
+  , trapDrums
 ) where
 
 import Euterpea
@@ -12,6 +13,16 @@ basicDrums = repeatM $ roll en $ perc ClosedHiHat 2
 accompanyingDrums :: Music Pitch
 accompanyingDrums = repeatM drums
   where
-    drums =  line [enr, enr, drumsHit sn, snr, enr, enr, enr, drumsHit en, enr]
+    drums = line [enr, enr, drumsHit sn, snr, enr, enr, enr, drumsHit en, enr]
 
     drumsHit = perc ElectricSnare
+
+trapDrums :: Music Pitch
+trapDrums = repeatM drums
+  where
+    drums = line [times 3 hit, snr, hit, snr, times 5 hit, snr, times 2 hit,
+      times 4 hhit]
+
+    hit = perc ClosedHiHat sn
+
+    hhit = perc ClosedHiHat tn
